49d408e646a21e14bf9e97d40edbb02a52680823
Dion Mark Corales <navybl4456@gmail.com>
2024-10-16 16:09:38 +0800

    Initial Commit
    Installed Vite.js for Frontend Tooling

---
+++ Leo_Frontend/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import buzcayaLogo from '/Logo Circle.png'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img src="${buzcayaLogo}" alt="">
+    <h1>Project Leo</h1>
+    <p>Coming Soon! On Development.</p>
+  </div>
+`
