04ff969233691de847ca76739466f0f6a2f5db16
Dion Mark Corales <navybl4456@gmail.com>
2024-10-22 20:18:54 +0800

    Updated Bootstrap
    Replaced Bootstrap.build's custom bootstrap file due to missing components, then replaced with new bootstrap built with Custom Sass (SCSS)

--- frontend/index.php
+++ frontend/index.php
@@ -1,93 +1,108 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Project Leoforeio</title>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="public/Logo%20Circle.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="src/css/bootstrap/bootstrap.min.css" />
-    <link rel="stylesheet" href="src/css/styles.css" />
-    <link rel="stylesheet" href="src/css/typography.css" />
-    <link rel="stylesheet" href="node_modules/@flaticon/flaticon-uicons/css/bold/rounded.css" />
-    <link rel="stylesheet" href="node_modules/@flaticon/flaticon-uicons/css/brands/all.css" />
+  <title>Project Leoforeio</title>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="public/Logo%20Circle.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="src/css/bootstrap/bootstrap.css" />
+  <link rel="stylesheet" href="src/css/typography.css" />
+  <link rel="stylesheet" href="src/css/custom.css" />
+  <link rel="stylesheet" href="node_modules/@flaticon/flaticon-uicons/css/bold/rounded.css" />
+  <link rel="stylesheet" href="node_modules/@flaticon/flaticon-uicons/css/brands/all.css" />
 </head>
 
 <body>
-  <?php 
-        include 'src/components/NavBar.php';
-        include  'src/components/SearchBar.php';
-        echo NavBar(
-          '<i class="fa fi-br-menu-burger mr-1"></i>', 'Menu',
-          '<i class="fa fi-br-user mr-1"></i>', 'Profile'
-        );
-        echo SearchBar('Search Destination...', '<i class="fi fi-br-search-location"></i>');
+  <?php
+  include 'src/components/NavBar.php';
+  include 'src/components/Card.php';
+
+  NavBar(
+    '<i class="fa fi-br-menu-burger"></i>',
+    'Menu',
+    '<i class="fa fi-br-user"></i>',
+    'Profile'
+  );
+
+  Card(
+    'Victory Liner #45 (AAA 001)',
+    'McDo Terminal',
+    'Quezon City, MNL',
+    '5PM October 19, 2024',
+    'P 100.00'
+  )
   ?>
-  <div class="container-fluid bg-dark text-light p-3">
-    <div class="row">
-      <div class="col">
-          <?php
-          include 'src/components/Card.php';
-          echo Card(
-                  'Victory Line #45 (AAA 001)',
-                  'McDo Terminal, Solano',
-                  'Quezon City, MNL',
-                  'October 15, 2024 5:00 PM',
-                  'P 100.00'
-          );
-          ?>
+  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</button>
 
-
-          <ul>
-              <li>List item 1</li>
-              <li>List item 2</li>
-              <li>List item 3</li>
-          </ul>
-
-          <ol>
-              <li>Ordered list item 1</li>
-              <li>Ordered list item 2</li>
-              <li>Ordered list item 3</li>
-          </ol>
-
-          <table class="table table-primary table-responsive">
-              <thead>
-              <tr>
-                  <th>Header 1</th>
-                  <th>Header 2</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                  <td>Data 1</td>
-                  <td>Data 2</td>
-              </tr>
-              </tbody>
-          </table>
-
-          <button class="btn btn-primary">Button</button>
-
-          <form>
-              <label for="input">Input:</label>
-              <input type="text" id="input" name="input" placeholder="Heeeeee" class="form-control bg-primary text-dark">
-              <div class="form-check">
-                  <input type="radio" class="form-check-input">
-                  <label class="form-check-label">Leoforeio</label>
-              </div>
-              <div class="form-check">
-                  <input type="checkbox" class="form-check-input" name="this"><label for="this" class="form-check-label">Leoforeio</label>
-              </div>
-              <input type="submit" class="btn btn-outline-light" value="Submit">
-          </form>
-
-          <div class="alert alert-warning alert-dismissable fade show" role="alert">
-              <h1>Danger</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>
+  <div class="container bg-dark text-light">
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <p>Try scrolling the rest of the page to see this option in action.</p>
       </div>
     </div>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+    <p>This is a paragraph. <a href="#">This is a link</a>.</p>
+    <div class="card text-bg-primary mb-3 p-2" style="max-width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Primary card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+    </ul>
+    <ol>
+      <li>Ordered list item 1</li>
+      <li>Ordered list item 2</li>
+      <li>Ordered list item 3</li>
+    </ol>
+    <table class="table table-primary table-responsive">
+      <thead>
+        <tr>
+          <th>Header 1</th>
+          <th>Header 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Data 1</td>
+          <td>Data 2</td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="btn btn-primary">Button</button>
+    <form>
+      <label for="input">Input:</label>
+      <input type="text" id="input" name="input" placeholder="Heeeeee" class="form-control bg-primary text-dark">
+      <div class="form-check">
+        <input type="radio" class="form-check-input"><label class="form-check-label">Leoforeio</label>
+      </div>
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input"><label class="form-check-label">Leoforeio</label>
+      </div>
+      <input type="submit" class="btn btn-outline-light" value="Submit">
+    </form>
+    <div class="alert alert-warning alert-dismissable fade show" role="alert">
+      <h1>Danger</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
   </div>
-  <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+
+
+  <!-- Include Popper.js and Bootstrap JavaScript -->
+  <script src="node_modules/@popperjs/core/dist/umd/popper.js"></script>
   <script src="src/js/bootstrap/bootstrap.js"></script>
 </body>
 
--- frontend/src/components/NavBar.php
+++ frontend/src/components/NavBar.php
@@ -1,16 +1,18 @@
-<?php 
-function NavBar($btn1_icon, $btn1_name, $btn2_icon, $btn2_name) {
-    echo "
+<?php
+function NavBar($btn1_icon, $btn1_name, $btn2_icon, $btn2_name)
+{
+  echo <<<HTML
     <div class='container-fluid p-2 bg-grey position-sticky fixed-top'>
         <div class='row'>
             <div class='col d-flex align-items-center'>
-                <button class='btn btn-primary btn-nav'>$btn1_icon $btn1_name</button>
+                <button class='btn btn-primary btn-nav' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasWithBothOptions' aria-controls='offcanvasWithBothOptions'>$btn1_icon $btn1_name</button>
             </div>
             <div class='col d-flex justify-content-end align-items-center'>
                 <button class='btn btn-primary btn-nav'>$btn2_icon $btn2_name</button>
             </div>
         </div>
     </div>
-    ";
+
+  HTML;
 }
 ?>
