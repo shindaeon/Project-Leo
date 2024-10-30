3b87daa43aa8b8db00807809b439b94f4ef82c6e
Dion Mark Corales <navybl4456@gmail.com>
2024-10-30 10:47:05 +0800

    Added Sign Up but with errors
    Uncaught (in promise) TypeError: NetworkError when attempting to fetch resource.

--- frontend/controllers/session_checker.php
+++ frontend/controllers/session_checker.php
@@ -2,6 +2,6 @@
 //checks if the user is logged in, if not, redirects to login page
 session_start();
 if ($_SESSION['username'] == null) {
-      header('Location: views/login.php');
+      header('Location: ../views/login.php');
       exit();
 }
